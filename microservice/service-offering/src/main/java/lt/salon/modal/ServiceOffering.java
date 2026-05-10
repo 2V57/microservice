25a985cbbb3fd29280c69439956802d34e46bffa
vitalij_rusakevic <vvitalijj@gmail.com>
2026-05-10 10:20:58 +0300

    Add ServiceOffering modal.

---
+++ microservice/service-offering/src/main/java/lt/salon/modal/ServiceOffering.java
@@ -0,0 +1,39 @@
+package lt.salon.modal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class ServiceOffering {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int duration;
+
+    @Column(nullable = false)
+    private Long salonId;
+
+    @Column(nullable = false)
+    private Long categoryId;
+
+    private String image;
+}
